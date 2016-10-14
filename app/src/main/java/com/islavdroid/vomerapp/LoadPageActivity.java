65cc7d95ec64a07a9b2f366f267423a4b0beeb76
islavstan <islavstan@gmail.com>
2016-10-14 10:33:37 +0300

    14.10.16

---
+++ app/src/main/java/com/islavdroid/vomerapp/LoadPageActivity.java
@@ -0,0 +1,22 @@
+package com.islavdroid.vomerapp;
+
+
+import android.os.Bundle;
+
+import android.support.v7.app.AppCompatActivity;
+
+import com.facebook.shimmer.ShimmerFrameLayout;
+
+
+public class LoadPageActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        setContentView(R.layout.load_page);
+        ShimmerFrameLayout container =
+                (ShimmerFrameLayout) findViewById(R.id.shimmer_view_container);
+        container.startShimmerAnimation();
+
+}}
